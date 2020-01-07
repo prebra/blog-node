0d30f446a8b3045fdacd47a75774895278b50c0d
prebra <1260886838@qq.com>
2020-01-07 18:20:52 +0800

    test

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -7,7 +7,7 @@ const con = mysql.createConnection(MYSQL_CONF)
 // 开始连接
 con.connect()
 
-// 统一执行sql的函数
+// 统一执行sql的函数.
 function exec(sql) {
   const promise = new Promise((resolve, reject) => {
     con.query(sql, (err, result) => {
